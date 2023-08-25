6c024ddd2575cd1a93f856dad2b1e5ae7c230527
ravkamaria <131477170+ravkamaria@users.noreply.github.com>
2023-08-25 15:37:31 +0000

    Deploying to gh-pages from @ ravkamaria/goit-js-hw-10@981f7eb17d81e731a980f89f4a6d9bcbdd2ac7d8 🚀

---
+++ index.260e499f.js
@@ -0,0 +1,2 @@
+document.querySelector(".breed-select");fetch("https://api.thecatapi.com/v1/images/search");
+//# sourceMappingURL=index.260e499f.js.map
---
+++ index.a61cb4ee.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".breed-select");fetch("https://api.thecatapi.com/v1/images/search")}();
+//# sourceMappingURL=index.a61cb4ee.js.map
