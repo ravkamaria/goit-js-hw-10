1afc6052b55294ae1612e7d3facb02dc2aa4f7d9
ravkamaria <hevkomaria1@gmail.com>
2023-08-26 21:25:49 +0300

    created function for search

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,7 @@
-export function fetchBreeds(){
-    return fetch(BASE_URL)
-    // .then(response => {
-    //     console.log(response)
-    //   })
-}
+// export function fetchBreeds(){
+//     return fetch(BASE_URL)
+//     // .then(response => {
+//     //     console.log(response)
+//     //   })
+// }
 
